7cbb51cb8d9a8509ce25d8eb65f82ddc5f6fd1a0
Gowtham Yarlagadda <gowtham777y@gmail.com>
2026-07-27 01:33:02 +0530

    Login route has been setup and access token is created when logged in

--- authentication/auth.py
+++ authentication/auth.py
@@ -2,12 +2,26 @@ from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+expire_minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
+SECRET_KEY = os.getenv("SECRET_KEY", "IwatchedOdesseyrecently")
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 def hash_password(password : str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hash_password: str) -> bool:
     return pwd_context.verify(plain_password,hash_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expire_minutes)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm="HS256")
--- router/users.py
+++ router/users.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database.database import get_db
 from database.models import UserModel
-from authentication.auth import hash_password, verify_password
+from authentication.auth import hash_password, verify_password, create_access_token
 
 class User(BaseModel):
     name: str
@@ -31,5 +32,10 @@ def signup(user: User, db: Session = Depends(get_db)):
     return {"message": "User has been Signed up"}
 
 @router.post("/login")
-def login(user: User,db : Session = Depends(get_db)):
-    return {"message": "User Logged in"}
+def login(form_data: OAuth2PasswordRequestForm = Depends(),db : Session = Depends(get_db)):
+    db_user = db.query(UserModel).filter(UserModel.email == form_data.username).first()
+    if not db_user or not verify_password(form_data.password,db_user.hashed_password):
+        raise HTTPException(status_code=404, detail="Invalid Username or Password")
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "type": "bearer"}
+
