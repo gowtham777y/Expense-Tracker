ca5a164b90e3fb9fe53116cf88733eeed3f3888b
Gowtham Yarlagadda <gowtham777y@gmail.com>
2026-07-27 11:51:32 +0530

    Created Expenses,Categories and Budget Tables

--- database/models.py
+++ database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Date, ForeignKey
+from sqlalchemy.orm import Relationship
 from database.database import Base
 
 class UserModel(Base):
@@ -9,3 +10,41 @@ class UserModel(Base):
     age = Column(Integer)
     email = Column(String,unique=True,index=True)
     hashed_password = Column(String)
+
+    expenses = Relationship("ExpenseModel",back_populates="owner")
+    category = Relationship("CategoryModel",back_populates="owner")
+    budget = Relationship("BudgetModel",back_populates="owner")
+
+class ExpenseModel(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer,primary_key=True,index=True)
+    name = Column(String)
+    category = Column(String)
+    description = Column(String)
+    date = Column(Date)
+    user_id = Column(Integer,ForeignKey("users.id"))
+
+    owner = Relationship("UserModel",back_populates="expenses")
+
+class CategoryModel(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer,primary_key=True,index=True)
+    category = Column(String)
+    user_id = Column(Integer,ForeignKey("users.id"))
+
+    owner = Relationship("UserModel",back_populates="category")
+    budget = Relationship("BudgetModel",back_populates="category")
+
+class BudgetModel(Base):
+    __tablename__ = "budgets"
+
+    id = Column(Integer,primary_key=True,index=True)
+    category = Column(String,ForeignKey("categories.category"))
+    budget = Column(Integer)
+    user_id = Column(Integer,ForeignKey("users.id"))
+
+    owner = Relationship("UserModel",back_populates="budget")
+    category = Relationship("CategoryModel",back_populates="budget")
+
