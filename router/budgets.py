f7c3d9e419b06a5ae866e14c3e49c72416628e85
Gowtham Yarlagadda <gowtham777y@gmail.com>
2026-07-27 14:22:28 +0530

    DELETE request for budget route has been added

--- router/budgets.py
+++ router/budgets.py
@@ -12,6 +12,10 @@ class Budget(BaseModel):
     month_name: str
     budget_amount: float
 
+class BudgetDel(BaseModel):
+    category_name: str
+    month_name: str
+
 router = APIRouter()
 
 def get_month_ranges(month_name : str):
@@ -76,4 +80,28 @@ def update_budget(budget: Budget, db: Session = Depends(get_db), current_user_em
     if not db_budget:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Budget doesn't exist")
     db_budget.budget = budget.budget_amount
+    db.commit()
+    db.refresh(db_budget)
     return {"status": status.HTTP_200_OK, "message": "Budget has been updated"}
+
+@router.delete("/budget")
+def delete_budget(budget: BudgetDel, db: Session = Depends(get_db), current_user_email: str= Depends(get_current_user)):
+    db_user = db.query(UserModel).filter(UserModel.email == current_user_email).first()
+    db_category = None
+    for categoryModel in db_user.category:
+        if categoryModel.category == budget.category_name:
+            db_category = categoryModel
+            break
+    if not db_category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category doesn't exist")
+    db_budget = None
+    start_date, end_date = get_month_ranges(budget.month_name)
+    for budgetModel in db_user.budget:
+        if budgetModel.category_id == db_category.id and budgetModel.start_date == start_date:
+            db_budget = budgetModel
+            break
+    if not db_budget:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Budget doesn't exist")
+    del db_budget
+    db.commit()
+    return {"message": "Budget Deleted"}
